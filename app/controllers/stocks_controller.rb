c4929606d6dc510c97a88c103bea31533c371d5d
gudimallaavinash0999 <gudimallaavinash0999@gmail.com>
2019-08-08 17:50:38 +0530

    updated search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,9 +3,14 @@ class StocksController < ApplicationController
     def search
       if params[:stock].present?
         @stock = Stock.new_from_lookup(params[:stock])
-        render 'users/my_portfolio'
+        if @stock
+          render 'users/my_portfolio'
+        else
+          flash[:danger]="you have enter incorrect symbol" 
+          redirect_to root_path
+        end
       else
-         flash[:danger]="please enter valied symbol"  
+         flash[:danger]="please enter symbol"  
          redirect_to root_path
       end
     end
